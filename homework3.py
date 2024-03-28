1050caf2b12dec1ea5211daf7353252ee8993623
Sergey-Ali <164060152+Sergey-Ali@users.noreply.github.com>
2024-03-28 17:17:22 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,16 @@
+my_string = 'Мой город'
+my_string = input('введите название города: ')
+my_string = "мой город Москва"
+print(len(my_string))
+my_string = 'Мой город Москва'
+my_string = my_string.upper()
+print(my_string)
+my_string = 'Мой город Москва'
+my_string = my_string.lower()
+print(my_string)
+print ('Мой город Москва'.replace(' ', ''))
+my_string = 'Мой город Москва'
+print(my_string[0:1])
+my_string = 'Мой город Москва'
+print(my_string[-1])
+print('Мой город-' + my_string)
